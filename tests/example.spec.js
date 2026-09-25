7ae88662ad19ace8d7294fd0fbaeda5f46692928
unknown <sreedarshan86@gmail.com>
2026-09-25 19:59:00 +0530

    Added comments

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -17,3 +17,20 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+//New - Created respoistory
+//Copy url
+//Local folder - created a folder
+//Right click - git bash within that folder
+//git clone copied url
+//opend the downloaded folder and open git bash within that folder - (main) can be seen
+//Added the folder to VS code
+//Installed playwright within that folder - all files in U status
+//In git bash, git status
+//git add filepath or git add .
+//git status to confirm all files are added
+//git commit -m "Message"
+//git push
+//create new branch in git hub
+//In git bash, git pull
+//git checkout <new_branch_name>
